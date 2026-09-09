3ea2ff3ab0eaf9b8112f736e250c16dd1d9865d3
riyasharma22062001-design <riyasharma22062001@gmail.com>
2026-09-09 11:18:00 +0530

    Fix null reference in board initialization

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ const winningConditions = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // cols  
     [0, 4, 8], [2, 4, 6] 
+    
 ];
 function checkWinner(player) {
     return winningConditions.some(condition => {
         return condition.every(index => board[index] === player);  
+        
     });
 }
 function isTie() {
+    
     return board.every(cell => cell !== null);    
 }
 function updateStatus() {
